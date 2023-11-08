f242a6f5d8c385f66b1bca6bbb0e05290056bcef
Joseph C <josephalexcobian@gmail.com>
2023-11-08 00:44:07 -0800

    Make cards be generated by JS instead of hard baked into html.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const things=document.querySelectorAll(".truncated");
-for (let thing of things) {
-   thing.addEventListener("click", ()=>{
-   thing.classList.toggle("truncated")})
-}
-
-// 
 const projects = document.querySelector(".projects");
 const newCard = document.querySelector("#new");
 newCard.addEventListener("click", ()=>{popupForm.style.display='grid'});
@@ -33,12 +26,23 @@ function createCard(project) {
     let cardImage = document.createElement("img");
     cardImage.setAttribute("src", project.imageURL);
     let cardDesc = document.createElement("p");
+    cardDesc.classList.add("truncated")
     cardDesc.textContent = project.description;
-    
-    card.appendChild(cardName);
-    card.appendChild(cardImage);
-    card.appendChild(cardDesc);
-    console.log(card);
+    // Buttons
+    let likeButton = document.createElement("input");
+    likeButton.setAttribute('src', './images/icons/thumb-up-outline.svg');
+    let ghButton = document.createElement("input");
+    ghButton.setAttribute('src', './images/icons/github.svg');
+    let shareButton = document.createElement("input");
+    shareButton.setAttribute('src', './images/icons/share.svg');
+    let buttons = [likeButton, ghButton, shareButton];
+    for (let button of buttons) {
+        button.setAttribute('type', 'image');
+        button.classList.add('icon');
+    }
+
+    card.append(cardName, cardImage, cardDesc, 
+        likeButton, ghButton, shareButton);
     projects.appendChild(card);
 }
 
@@ -51,3 +55,65 @@ submitButton.addEventListener("click", ()=>{
     createCard(temp);
     popupForm.style.display='none'
 })
+
+//data
+let userData = {
+    firstName: "joseph",
+    lastName: "cobian",
+    handle: "joseph",
+    pfp: "./images/pfp.jpeg",
+    projects: [
+        {
+            name: "Pumpkin Patch",
+            imageURL: './images/pumpkin.jpeg',
+            description: "Website advertising for a pumpkin patch. Features a Halloween countdown, a ticket page that adds selected ticket prices, and a contact page with a form for submitting a message.",
+            likes: 0,
+            liked: false,
+        },
+        {
+            name: "Calculator",
+            imageURL: './images/calculator.jpg',
+            description: "A calculator in the style of an oldschool gameboy color. Supports basic operations including modulus.",
+            likes: 0,
+            liked: false,
+        },
+        {
+            name: "Etch a Sketch",
+            imageURL: './images/EAS.jpeg',
+            description: "A drawing website that changes the colors of a grid based on if the user hovers over it with their mouse. Includes a custom animated toggle button to switch from rgb colors to a more pastel pallette.",
+            likes: 0,
+            liked: false,
+        },
+        {
+            name: "Hacking Simulator",
+            imageURL: './images/hac.jpeg',
+            description: "Simulates being a hackerman by converting user keypresses to gibberish hacking terms. Includes a typewriter effect that makes text appear one letter at a time as if it is being typed.",
+            likes: 0,
+            liked: false,
+        },
+        {
+            name: "Rock Paper Scissors",
+            imageURL: './images/RPS.jpeg',
+            description: "A simple game of rock paper scissors styled after the card game Inscryption. The user chooses a card and the opponent bot's card is flipped and revealed.",
+            likes: 0,
+            liked: false,
+        },
+        {
+            name: "Full Moon Space Travel",
+            imageURL: './images/FMS.jpeg',
+            description: "A fictional space tourism company.",
+            likes: 0,
+            liked: false,
+        },
+    ]
+}
+
+for (let project of userData.projects) {
+    createCard(project);
+}
+
+const things=document.querySelectorAll(".truncated");
+for (let thing of things) {
+   thing.addEventListener("click", ()=>{
+   thing.classList.toggle("truncated")})
+}
